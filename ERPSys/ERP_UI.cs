70b6a023342733dadb813b998ae9c699415f1da7
Casper Simon Jensen <casp142b@elev.techcollege.dk>
2024-05-01 10:42:49 +0200

    V1:UI menu added

---
+++ ERPSys/ERP_UI.cs
@@ -0,0 +1,31 @@
+﻿using ERPSys;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TECHCOOL.UI;
+
+namespace ERPSys
+{
+    public class ERP_UI : Screen
+    {
+        public override string Title { get; set; } = "Menu";
+        protected override void Draw()
+        {
+            Clear(this);
+            Menu menu = new Menu();
+            menu.Add(new Companylists());
+            
+            menu.Start(this);
+        }
+    }
+    public class Companylists : Screen
+    {
+        public override string Title { get; set; } = "Company List";
+        protected override void Draw()
+        {
+
+        }
+    }
+}
