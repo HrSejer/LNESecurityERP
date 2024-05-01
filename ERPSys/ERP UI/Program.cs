79c2b41875df74e35bdc4d2dfd62f02ec2036731
Casper Simon Jensen <casp142b@elev.techcollege.dk>
2024-05-01 14:41:41 +0200

    V1-2: Moved into a folder to orginise it

---
+++ ERPSys/ERP UI/Program.cs
@@ -0,0 +1,15 @@
+﻿using ERPSys;
+using TECHCOOL.UI;
+
+namespace ERPSys
+{ 
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        
+            ERP_UI firstScreen = new ERP_UI();
+            Screen.Display(new ERP_UI());
+        }
+    }
+}
