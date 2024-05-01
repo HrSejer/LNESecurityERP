822900fead92d860b7bb8490e7ca3e9415e3c0a2
Casper Simon Jensen <casp142b@elev.techcollege.dk>
2024-05-01 08:30:50 +0200

    D1: made the currency enum

---
+++ LNESecurityERP/CurrencyClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LNESecurityERP
+{
+    public class CurrencyClass
+    {
+        //enum for currency
+        public enum Currency
+        {
+            DKK, 
+            SEK, 
+            USD, 
+            EUR  
+        }
+    }
+}
