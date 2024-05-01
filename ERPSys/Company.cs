79002ec45a4a207ddcd02124b37389f97977ddb9
Casper Simon Jensen <casp142b@elev.techcollege.dk>
2024-05-01 13:36:23 +0200

    V1: Got the UI to show a list of companies

---
+++ ERPSys/Company.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ERPSys
+{
+    internal class Company
+    {
+        public string CompanyName { get; set; } = "";
+        public string Vej { get; set;}
+        public string Husnummer { get; set; }
+        public string Postnummer { get; set; }
+        public string By { get; set; }
+        public string Land {  get; set; }
+        public string Currency {  get; set; }
+        public Company() { }
+        public Company(string companyName, string vej, string husnummer, string postnummer, string by, string land, string currency)
+        {
+            CompanyName = companyName;
+            Vej = vej;
+            Husnummer = husnummer;
+            Postnummer = postnummer;
+            By = by;
+            Land = land;
+            Currency = currency;
+        }
+    }
+}
--- ERPSys/ERP_UI.cs
+++ ERPSys/ERP_UI.cs
@@ -10,13 +10,17 @@ namespace ERPSys
 {
     public class ERP_UI : Screen
     {
+        //er main menu til vores UI
         public override string Title { get; set; } = "Menu";
         protected override void Draw()
         {
+            
             Clear(this);
             Menu menu = new Menu();
+            //=====UI referencer====//
             menu.Add(new Companylists());
-            
+
+            //=====UI referencer====//
             menu.Start(this);
         }
     }
@@ -25,7 +29,18 @@ namespace ERPSys
         public override string Title { get; set; } = "Company List";
         protected override void Draw()
         {
+            Clear(this);
+            Menu menu = new Menu();
 
+            ListPage<Company> companylist = new();
+            companylist.Add(new Company("Company", "Someroad","5","9303","city","companycountry", "DKK"));
+            companylist.Add(new Company("Company1", "Someroad", "5", "9303", "city", "companycountry1", "DKK1"));
+            companylist.Add(new Company("Company2", "Someroad", "5", "9303", "city", "companycountry2", "DKK2"));
+
+            companylist.AddColumn("Company Name", "CompanyName");
+            companylist.AddColumn("Country", "Land");
+            companylist.AddColumn("Currency", "Currency");
+            companylist.Draw();
         }
     }
 }
