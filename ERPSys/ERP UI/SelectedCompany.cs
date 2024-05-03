66ee955fa79a2beccc849af6cd1ed2decd4f5045
Casper Simon Jensen <casp142b@elev.techcollege.dk>
2024-05-03 10:20:01 +0200

    V1-2: Moved all UI classes into theire own Class file

---
+++ ERPSys/ERP UI/SelectedCompany.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TECHCOOL.UI;
+
+namespace ERPSys
+{
+    public class SelectedCompany(Company company) : Screen
+    {
+        //made to show a single company's information
+        public override string Title { get; set; } = "Selected Company";
+        protected override void Draw()
+        {
+            Clear(this);
+
+            ListPage<Company> selectedCompany = new ListPage<Company>();
+            selectedCompany.Add(company);
+            selectedCompany.AddColumn("Company Name", "CompanyName");
+            selectedCompany.AddColumn("Country", "Land");
+            selectedCompany.AddColumn("Currency", "Currency");
+            selectedCompany.AddColumn("Husnummer", "Husnummer");
+            selectedCompany.AddColumn("Postnummer", "Postnummer");
+            selectedCompany.AddColumn("By", "By");
+
+            selectedCompany.Draw();
+
+            ExitOnEscape();
+        }
+    }
+}
