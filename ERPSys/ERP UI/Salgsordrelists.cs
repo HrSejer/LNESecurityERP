f8dca8ff72af5e53fb3f59d2f7e3f55ab6db1aa9
Christian Nielsen <120013949+HrSejer@users.noreply.github.com>
2024-05-06 10:17:06 +0200

    S2-Lavet et seperat Class til Salgordrelists

---
+++ ERPSys/ERP UI/Salgsordrelists.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TECHCOOL.UI;
+
+namespace ERPSys
+{
+    public class SalgsordreLists : Screen
+    {
+        public override string Title { get; set; } = "Salgsordre list";
+        protected override void Draw()
+        {
+            do
+            {
+                ListPage<Salgsordrehoved> salgsordreList = new();
+                salgsordreList.Add(new Salgsordrehoved(1, 1, "Oprettet", 100));
+            }
+            while (Show);
+        }
+    }
+}
