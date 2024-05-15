7192d39dc7b5551e79f1427cba79cf081485ed66
Casper Simon Jensen <casp142b@elev.techcollege.dk>
2024-05-15 09:22:57 +0200

    V6-7: fik lavet en opening UI som viser firma navn og der efter fik lavet en måde at delete et company

---
+++ ERPSys/Data/Opening UI.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TECHCOOL.UI;
+
+namespace ERPSys.Data
+{
+    public class Opening_UI:Screen
+    {
+        public override string Title { get; set; } = "LNE SECURITY A/S";
+        protected override void Draw()
+        {
+            Console.CursorVisible = false;
+            Clear(this);
+            Menu menu = new Menu();
+            menu.Add(new ERP_Menu());
+            menu.Start(this);
+        }
+    }
+}
